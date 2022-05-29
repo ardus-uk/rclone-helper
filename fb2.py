53b50c0bf34cd99c4930dfcc9b8b1cc4605de1c3
ardus-uk <peter@ardus.co.uk>
2022-05-29 20:13:01 +0100

    Modify selection to default to cwd

--- fb2.py
+++ fb2.py
@@ -4,28 +4,33 @@ import PySimpleGUI as sg
 import sys
 import os
 
-if len(sys.argv) == 1:
-    event, values = sg.Window('My Script',
-                    [[sg.Text('Select local directory')],
-                    [sg.In(), sg.FolderBrowse()],
-                    [sg.Open(), sg.Cancel()]]).read(close=True)
+
+event, values = sg.Window('Local Directory Selection',
+                [[sg.Text('Select local directory')],
+                [sg.In(), sg.FolderBrowse()],
+                [sg.Open(), sg.Cancel()]]).read(close=True)
+if values[0]:
     fname = values[0]
 else:
-    fname = sys.argv[1]
+    fname = os.getcwd()
+
+remote = fname.split("/")
+print(remote)
+
 
 if not fname:
     sg.popup("Cancel", "No directory name supplied")
     raise SystemExit("Cancelling: no directory name supplied")
 else:
     layout = [[sg.Text("Local: "+fname)],
-          [sg.Text("Remote: BB-wcs-US:"+os.path.basename(fname))],
-          [sg.Text("DIRECTION-------------------------------------------")],
+          [sg.Text("Remote: "+remote[4]+":"+os.path.basename(fname))],
+          [sg.Text("DIRECTION------------------------------------------------")],
           [sg.T("         "), sg.Radio('Local to Remote', "RADIO1", default=True, key="-IN1-")],
           [sg.T("         "), sg.Radio('Remote to Local', "RADIO1", default=False)],
-          [sg.Text("COPY/CLONE------------------------------------------")],         
+          [sg.Text("COPY/CLONE---------------------------------------------")],         
           [sg.T("         "), sg.Radio('Copy', "RADIO2", default=True, key="-IN2-")],
           [sg.T("         "), sg.Radio('Clone', "RADIO2", default=False)],
-          [sg.Text("-------------------------------------------")],
+          [sg.Text("---------------------------------------------------------------")],
           [sg.T("         "), sg.Checkbox('Selection active', default=False, key="-SEL-")],
           [sg.T("")],[sg.T("        "), sg.Button("Action",size=(12,4))], [sg.T("")]]
 
