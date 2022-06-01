9d2b1259c00bd5d9b45da95fc48a6839eec7ebd0
ardus-uk <peter@ardus.co.uk>
2022-06-01 17:57:10 +0100

    Add actual action

--- fb2.py
+++ fb2.py
@@ -17,52 +17,72 @@ if values[0]:
 else:
     fname = os.getcwd()
 
+if not os.path.isdir(fname):
+    sg.popup(fname+" is not a valid directory")
+    raise SystemExit("Cancelling - not a valid directory") 
 
 remote_path_parts = fname.split("/")
+print(len(remote_path_parts))
 print(remote_path_parts)
 
-remote = remote_path_parts[4]+":"
-print(remote)
+remote = remote_path_parts[4]
+remote_label = remote+":"
+if len(remote_path_parts)>5:
+    remote_tree = "/".join(remote_path_parts[-(len(remote_path_parts)-5):])
+else:
+    remote_tree = "/"+remote
 
-if remote not in valid_remotes:
-    sg.popup(remote+" is not a valid Backblaze bucket")
+print(remote)
+print(remote_tree)
+
+if remote_label not in valid_remotes:
+    sg.popup(remote_label+" is not a valid Backblaze bucket")
     raise SystemExit("Cancelling - not a valid Backblaze bucket")   
 
 if not fname:
     sg.popup("Cancel", "No directory name supplied")
     raise SystemExit("Cancelling: no directory name supplied")
-else:
-    layout = [[sg.Text("Local: "+fname)],
-          [sg.Text("Remote: "+remote+os.path.basename(fname))],
+
+remote_complete_description = remote_label+remote_tree
+
+layout = [[sg.Text("Local: "+fname)],
+          [sg.Text("Remote: "+remote_complete_description)],
           [sg.Text("DIRECTION------------------------------------------------")],
-          [sg.T("         "), sg.Radio('Local to Remote', "RADIO1", default=True, key="-IN1-")],
-          [sg.T("         "), sg.Radio('Remote to Local', "RADIO1", default=False)],
+          [sg.T("         "), sg.Radio('Local to Remote', "DIRCTN", default=True, key="-IN1-")],
+          [sg.T("         "), sg.Radio('Remote to Local', "DIRCTN", default=False)],
           [sg.Text("COPY/CLONE---------------------------------------------")],         
-          [sg.T("         "), sg.Radio('Copy', "RADIO2", default=True, key="-IN2-")],
-          [sg.T("         "), sg.Radio('Clone', "RADIO2", default=False)],
+          [sg.T("         "), sg.Radio('Copy', "COSY", default=True, key="-IN2-")],
+          [sg.T("         "), sg.Radio('Sync', "COSY", default=False)],
           [sg.Text("---------------------------------------------------------------")],
           [sg.T("         "), sg.Checkbox('Selection active', default=False, key="-SEL-")],
           [sg.T("")],[sg.T("        "), sg.Button("Action",size=(12,4))], [sg.T("")]]
 
 ###Setting Window
-    window = sg.Window('rclone assistant', layout, size=(500,400))
+window = sg.Window('rclone assistant', layout, size=(500,400))
 
 ###Showing the Application, also GUI functions can be placed here.
 
-    while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED or event=="Exit":
-            break
-        if values["-SEL-"] == True:
-            if values["-IN1-"] == True:
-                if values["-IN2-"] == True:
-                    print("Local to Remote Copy")
-                else:
-                    print("Local to Remote Clone")
-            else:
-                if values["-IN2-"] == True:
-                    print("Remote to Local Copy")
-                else:
-                    print("Remote to Local Clone")
+event, values = window.read()
+
+if event == sg.WIN_CLOSED or event=="Exit":
+    raise SystemExit("Window closed")
+
+if values["-SEL-"] == True:
+    if values["-IN1-"] == True:
+        if values["-IN2-"] == True:
+            print("Local to Remote Copy")
+            cmd = "rclone copy "+fname+" "+remote_complete_description
+        else:
+            print("Local to Remote Sync")
+            cmd = "rclone sync "+fname+" "+remote_complete_description
+    else:
+        if values["-IN2-"] == True:
+            print("Remote to Local Copy")
+            cmd = "rclone copy "+remote_complete_description+" "+fname
+        else:
+            print("Remote to Local Sync")
+            cmd = "rclone sync "+remote_complete_description+" "+fname
+    print(cmd)
+    subprocess.getoutput(cmd)
             
-    window.close()
+window.close()
